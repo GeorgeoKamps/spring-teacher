f8580bcd2a1ef858815884a4edfe4a6502cd7991
Georgios Kampouris <georgeokamps@gmail.com>
2023-09-05 21:18:25 +0300

    Add files via upload

---
+++ spring-teacher4/src/main/java/gr/aueb/cf/springteacher4/dto/BaseDTO.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.springteacher4.dto;
+
+import javax.validation.constraints.NotNull;
+
+public abstract class BaseDTO {
+    @NotNull
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
